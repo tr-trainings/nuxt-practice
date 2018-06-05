e9df37e4d4ab763705bedc778a11edbdd1dc38c5
Leonard Dalmacio <leonarddalmacio@Aracari-2.local>
2018-06-05 15:24:23 +0800

    added the axios all

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -6,7 +6,8 @@
             div.card.m-b-1(v-for="post in posts")
                 div.box
                     div.card-header 
-                        div.card-header-title Title: {{post.title}}
+                        div.card-header-title Title: {{post.title}} 
+                        span by: {{ post.name }}
                     div.card-content
                         p {{ post.body }}
                         br
@@ -21,13 +22,30 @@ export default {
     components: {
         appHeader: Header
     },
-    asyncData ({ params }) {
-        return axios.get(`https://jsonplaceholder.typicode.com/posts/`)
-        .then((res) => {
-            return {
-                posts: res.data
+    async asyncData ({ params }) {
+        // get all posts from jsonplaceholder.typicode.com/post/
+        let posts = await axios.get(`https://jsonplaceholder.typicode.com/posts/`).then((res) => {
+            return res;
+        })
+
+        // use the userId from the fetched posts to locate the name of the user
+        let injectToPosts = [];
+        posts.data.forEach((rawPost, index) => {
+            let toInject = axios.get(`https://jsonplaceholder.typicode.com/users/${rawPost.userId}`)
+            injectToPosts.push(toInject);
+        });
+
+        //injection the name of the user to posts
+        await axios.all(injectToPosts).then((result) => {
+            for (let i = 0; i < result.length; i++){
+                posts.data[i].name = result[i].data.name;
             }
         })
+
+        // sending
+        return {
+            posts: posts.data
+        }
     }
 }
 </script>

88f058917567d2d206777824fee4c4d8889d4d31
Leonard Dalmacio <leonarddalmacio@Aracari-2.local>
2018-06-14 09:43:29 +0800

    updated nuxt tutorial

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,28 @@
 <template lang="pug">
-	nav.navbar.is-primary.has-shadow.is-spaced
-		div.container
-			div.navbar-brand
-				a.nav-item.is-size-3.has-text-light </> Blog Posts
-			div.navbar-menu
-				div.navbar-start.m-l-2
-					nuxt-link.navbar-item.button.is-primary.control(to="/") Home
-					nuxt-link.navbar-item.button.is-primary.control(to="/posts") Posts
-					nuxt-link.navbar-item.button.is-primary.control(to="/basic-routes") Basic Routes
-					nuxt-link.navbar-item.button.is-primary.control(to="/dynamic-routes") Dynamic Routes
-					nuxt-link.navbar-item.button.is-primary.control(to="/nested-routes/users") Nested Routes
+  div
+    nav.navbar.is-primary.has-shadow.is-spaced.is-fixed-top
+      div.container
+        div.navbar-brand
+          a.nav-item.is-size-3.has-text-light </> Blog Posts
+        div.navbar-menu
+          div.navbar-start.m-l-2
+            nuxt-link.navbar-item.button.is-primary.control(to="/") Home
+            nuxt-link.navbar-item.button.is-primary.control(to="/posts") Posts
+            nuxt-link.navbar-item.button.is-primary.control(to="/basic-routes") Basic Routes
+            nuxt-link.navbar-item.button.is-primary.control(to="/dynamic-routes") Dynamic Routes
+            nuxt-link.navbar-item.button.is-primary.control(to="/nested-routes/users") Nested Routes
+    nav.navbar.is-primary.has-shadow.is-spaced.push-down
+      div.container
+        div.navbar-brand
+          a.nav-item.is-size-3.has-text-light </> Blog Posts
 </template>
 
 <style lang="scss">
 .m-l-2 {
 	margin-left: 2rem;
 }
-.navbar { 
-  position: fixed !important; 
-  top: 0; left: 0; 
-  right: 0; 
+.push-down {
+  z-index: -1;
 }
 </style>
 
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,68 +1,68 @@
 <template lang="pug">
-    div
-        img.has-bg-img
-        div.container
-            div.card.m-b-1(v-for="post in posts")
-                div.box
-                    div.card-header 
-                        div.card-header-title Title: {{post.title}} 
-                        span by: {{ post.name }}
-                    div.card-content
-                        p {{ post.body }}
-                        br
-                        nuxt-link(:to="'/posts/'+ post.id") Read more...
+div
+	img.has-bg-img
+	div.container
+		div.card.m-b-1(v-for="post in posts")
+			div.box
+				div.card-header
+					div.card-header-title Title: {{post.title}}
+					span by: {{ post.name }}
+				div.card-content
+					p {{ post.body }}
+					br
+					nuxt-link(:to="'/posts/'+ post.id") Read more...
 </template>
 
 <script>
-import axios from 'axios';
+import  axios  from  'axios';
 
-export default {
-    async asyncData ({ params }) {
-        // get all posts from jsonplaceholder.typicode.com/post/
-        let posts = await axios.get(`https://jsonplaceholder.typicode.com/posts/`).then((res) => {
-            return res;
-        })
+export  default {
+	async  asyncData ({ params }) {
+		// get all posts from jsonplaceholder.typicode.com/post/
+		let  posts  =  await  axios.get(`https://jsonplaceholder.typicode.com/posts/`).then((res) => {
+			return  res;
+		})
 
-        // use the userId from the fetched posts to locate the name of the user
-        let injectToPosts = [];
-        posts.data.forEach((rawPost, index) => {
-            let toInject = axios.get(`https://jsonplaceholder.typicode.com/users/${rawPost.userId}`)
-            injectToPosts.push(toInject);
-        });
+		// use the userId from the fetched posts to locate the name of the user
+		let  injectToPosts  = [];
+		posts.data.forEach((rawPost, index) => {
+			let  toInject  =  axios.get(`https://jsonplaceholder.typicode.com/users/${rawPost.userId}`)
+			injectToPosts.push(toInject);
+		});
 
-        //injection the name of the user to posts
-        await axios.all(injectToPosts).then((result) => {
-            for (let i = 0; i < result.length; i++){
-                posts.data[i].name = result[i].data.name;
-            }
-        })
+		//injection the name of the user to posts
+		await  axios.all(injectToPosts).then((result) => {
+			for (let  i  =  0; i  <  result.length; i++){
+				posts.data[i].name  =  result[i].data.name;
+			}
+		})
 
-        // sending
-        return {
-            posts: posts.data
-        }
-    }
+		// sending
+		return {
+			posts:  posts.data
+		}
+	}
 }
 </script>
 
-<style lang="scss">
+<style  lang="scss">
 .m-b-1 {
-    margin-bottom: 1rem;
+	margin-bottom: 1rem;
 }
 .box {
-    background: rgba(255,255,255, 0.6);
+	background: rgba(255,255,255, 0.6);
 }
 .card {
-    background: rgba(255,255,255, 0.6);
-    color: rgba(0,0,0,1);
+	background: rgba(255,255,255, 0.6);
+	color: rgba(0,0,0,1);
 }
 .has-bg-img {
-    background: url('~/assets/bg-img.jpg');
-    background-size: cover;
-    background-position: top;
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
+	background: url('~/assets/bg-img.jpg');
+	background-size: cover;
+	background-position: top;
+	position: fixed;
+	height: 100vh;
+	width: 100vw;
 }
 </style>
 
